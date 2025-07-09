e906410ba948d8a61203d4eaedf63d6d6e2aa7e3
quantumBpB <dr.darpan.anand@gmail.com>
2025-07-09 21:34:35 +0530

    done

--- Test.py
+++ Test.py
@@ -20,6 +20,7 @@ def plot_bloch_spheres(state, title_prefix):
         b.add_vectors(bloch_vec)
         b.title = f"{title_prefix} Qubit {i}"
         b.render()
+        b.save("ERR.jpg")
 
 # Step 1: Initial state (|0000>)
 qc = QuantumCircuit(4)
